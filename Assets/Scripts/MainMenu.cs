74448aff19a8a0c5954ee1937da89afb3522b234
TheTLV <phuhn1080@gmail.com>
2025-10-01 11:01:12 +0700

    mm

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("ChooseLevel"); // load màn chơi đầu tiên
+    }
+
+    public void OpenSettings(GameObject settingsPanel)
+    {
+        settingsPanel.SetActive(true); // bật panel Settings
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit Game!"); // chỉ hiện trong editor
+    }
+}
+ 
